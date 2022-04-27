1a8ef8748bc2db7d67676bbbde78cb3a8d6dd2f5
PeedieOrc <77984879+PeedieOrc@users.noreply.github.com>
2022-04-27 23:13:48 +0100

    New Clock project

---
+++ src/clock/View.java
@@ -0,0 +1,63 @@
+package clock;
+
+import java.awt.*;
+import javax.swing.*;
+import java.util.Observer;
+import java.util.Observable;
+
+public class View implements Observer {
+    
+    ClockPanel panel;
+    
+    public View(Model model) {
+        JFrame frame = new JFrame();
+        panel = new ClockPanel(model);
+        //frame.setContentPane(panel);
+        frame.setTitle("Java Clock");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // Start of border layout code
+        
+        // I've just put a single button in each of the border positions:
+        // PAGE_START (i.e. top), PAGE_END (bottom), LINE_START (left) and
+        // LINE_END (right). You can omit any of these, or replace the button
+        // with something else like a label or a menu bar. Or maybe you can
+        // figure out how to pack more than one thing into one of those
+        // positions. This is the very simplest border layout possible, just
+        // to help you get started.
+        
+        Container pane = frame.getContentPane();
+//Set up project first - get using github through jetbeans
+//start here - amend so that left hand side creates the alarms - right side displays them
+//bottom button to set the alarm - Look at creating drop down - type menctioned in lecture slides 
+//once complete read data in - will need to decide on the data type
+//could just compare the date and time - can set this so that its set by default todays date, if hours < current hours then next day
+// then need to send to array list for storage (use default for ease since my lists not completed. - will need to amend it so that it does it in reverse order and stores as date
+// can then call list when new alarm is set or when button is pressed - can output list with already existing call
+
+        
+        JButton button = new JButton("Button 1 (PAGE_START)");
+        pane.add(button, BorderLayout.PAGE_START);
+         
+        panel.setPreferredSize(new Dimension(200, 200));
+        pane.add(panel, BorderLayout.CENTER);
+         
+        button = new JButton("Button 3 (LINE_START)");
+        pane.add(button, BorderLayout.LINE_START);
+         
+        button = new JButton("Long-Named Button 4 (PAGE_END)");
+        pane.add(button, BorderLayout.PAGE_END);
+         
+        button = new JButton("5 (LINE_END)");
+        pane.add(button, BorderLayout.LINE_END);
+        
+        // End of borderlayout code
+        
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    public void update(Observable o, Object arg) {
+        panel.repaint();
+    }
+}
