51c2d2895b3d7e28cfe509ee5d25ef4d278fc9a6
PeedieOrc <77984879+PeedieOrc@users.noreply.github.com>
2022-05-01 15:48:36 +0100

    Restarted from Scratch
    Added Menu
    Figured out action listener however feel this isn't fully correct but moving on as spent too long on it.
    Simplified code

--- src/clock/View.java
+++ src/clock/View.java
@@ -1,23 +1,26 @@
 package clock;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import javax.swing.*;
 import java.util.Observer;
 import java.util.Observable;
 
 public class View implements Observer {
-    
+
     ClockPanel panel;
-    
+
     public View(Model model) {
+
         JFrame frame = new JFrame();
         panel = new ClockPanel(model);
         //frame.setContentPane(panel);
         frame.setTitle("Java Clock");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         // Start of border layout code
-        
         // I've just put a single button in each of the border positions:
         // PAGE_START (i.e. top), PAGE_END (bottom), LINE_START (left) and
         // LINE_END (right). You can omit any of these, or replace the button
@@ -25,64 +28,94 @@ public class View implements Observer {
         // figure out how to pack more than one thing into one of those
         // positions. This is the very simplest border layout possible, just
         // to help you get started.
-        
         Container pane = frame.getContentPane();
-//Set up project first - get using github through jetbeans
-//start here - amend so that left hand side creates the alarms - right side displays them
-//bottom button to set the alarm - Look at creating drop down - type menctioned in lecture slides 
-//once complete read data in - will need to decide on the data type
-//could just compare the date and time - can set this so that its set by default todays date, if hours < current hours then next day
-// then need to send to array list for storage (use default for ease since my lists not completed. - will need to amend it so that it does it in reverse order and stores as date
-// can then call list when new alarm is set or when button is pressed - can output list with already existing call
 
-        
-        JButton button = new JButton("Need something here or it crashes?");
-        pane.add(button, BorderLayout.PAGE_START);
-         
+        JButton aboutButton = new JButton("About");
+        pane.add(aboutButton, BorderLayout.PAGE_START);
+        aboutButton.addActionListener(new AboutClock(this));
+
         panel.setPreferredSize(new Dimension(200, 200));
         pane.add(panel, BorderLayout.CENTER);
-         
-        //button = new JButton("Button 3 (LINE_START)");
-        //pane.add(button, BorderLayout.LINE_START);
 
+        //JButton button = new JButton("Button 3 (LINE_START)");
+       //pane.add(button, BorderLayout.LINE_START);
         JPanel alarmClockSet = new JPanel();
         alarmClockSet.setPreferredSize(new Dimension(150, 200)); 
         pane.add(alarmClockSet,BorderLayout.LINE_START);
         //#TODO must be a better way to store the numbers for the drop down - either using loop or internal timer? 
-        String hours[]={"1","2","3","4","5","6","7","8","9","10","11","12"};
+        String hours[]={"1","2","3","4","5","6","7","8","9","11","12","13","14","15","16","17","18","19","20","21","22","23",};
         String mins[] = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59"}; 
-        JComboBox bcHours =new JComboBox(hours);
-        JComboBox bcMins =new JComboBox(mins);
+        String message = "Hello World";
+        final JComboBox bcHours = new JComboBox(hours);
         alarmClockSet.add(bcHours, BorderLayout.CENTER);
+        final JComboBox bcMins = new JComboBox(mins);
         alarmClockSet.add(bcMins, BorderLayout.LINE_START);
-        button = new JButton("Set Alarm");
-        alarmClockSet.add(button, BorderLayout.PAGE_END);
+        JButton alarmbutton = new JButton("Set Alarm");
+        alarmClockSet.add(alarmbutton, BorderLayout.PAGE_END);
+        alarmbutton.setToolTipText("Click this button to add an alarm for the set time");
+        //Will only pass when initilised - will not send latest selection. Action listener on an action listner?
+        //alarmbutton.addActionListener(new SetAlarmHandler(bcHours, this));
+        alarmbutton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                String hours = bcHours.getSelectedItem().toString();
+                String mins = bcMins.getSelectedItem().toString();
+                System.out.println("Current Alarm: " + hours + ":" + mins);
+            //pass the below into sorted array 
+                
+            }
+        });
+
+        JButton button = new JButton("Long-Named Button 4 (PAGE_END)");
+        pane.add(button, BorderLayout.PAGE_END);
+
+        button = new JButton("5 (LINE_END)");
+        pane.add(button, BorderLayout.LINE_END);
+
+        //menu - Could end up putting this in its own class... 
+        JMenuBar menuBar;
+        JMenu menu, submenu;
+        JMenuItem menuItem;
+        //add file
+        menuBar = new JMenuBar();
+        menu = new JMenu("File");
+        menu.setMnemonic(KeyEvent.VK_A);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "Add description here");
+        menuBar.add(menu);
+        //add exit 
+        JMenuItem exitItem = new JMenuItem("Exit");
+        exitItem.setMnemonic('x');
+        class ExitItemListener implements ActionListener {
+
+            public void actionPerformed(ActionEvent event) {
+                System.exit(0);
+            }
+        }
+        exitItem.addActionListener(new ExitItemListener());
+        menuBar.add(exitItem);
+        //add about below file
+        menu.addSeparator();
+        menuItem = new JMenuItem("About",
+                KeyEvent.VK_T);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_1, ActionEvent.ALT_MASK));
+        menuItem.getAccessibleContext().setAccessibleDescription(
+                "Information for the about");
+        menuItem.addActionListener(new AboutClock(this));
+        menu.add(menuItem);
+        frame.setJMenuBar(menuBar);
 
 
-        //button = new JButton("LETS FUCKING GO OW2!");
-        //pane.add(button, BorderLayout.PAGE_END);
-        //TODO variable name
-        
-        //button = new JButton("5 (LINE_END)");
-        //pane.add(button, BorderLayout.LINE_END);
-        DefaultListModel<String> test = new DefaultListModel<>();
-          test.addElement("Alarms Set");          
-          test.addElement("01:00 pm");  
-          test.addElement("05:00 am");  
-          test.addElement("06:00 pm");  
-          test.addElement("09:00 am");
-        JList<String> list = new JList<>(test); 
-        list.setPreferredSize(new Dimension(100, 200));
-        pane.add(list, BorderLayout.LINE_END);
 
-        
+
         // End of borderlayout code
-        
+
         frame.pack();
         frame.setVisible(true);
     }
-    
+
     public void update(Observable o, Object arg) {
         panel.repaint();
     }
+
 }
