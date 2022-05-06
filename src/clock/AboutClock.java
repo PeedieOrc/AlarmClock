b35b7f6b137e89dbb8e346df2bf02c2c23fb53fa
PeedieOrc <77984879+PeedieOrc@users.noreply.github.com>
2022-05-06 20:18:16 +0100

    Update to the documentation

--- src/clock/AboutClock.java
+++ src/clock/AboutClock.java
@@ -10,16 +10,27 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 /**
- *
- * @author Terra
+ * this lass will open the about window and display the below message to the user
+ * @author Ray Banks
+ * @version 1.2    
+ * @date 06/05/2022
  */
 public class AboutClock implements ActionListener {
     private String about;
 
+    /**
+     *
+     * @param aThis
+     */
     public AboutClock(View aThis) {
         about = "This clock will only go from the head item in the array queue. Created by Ray Banks. 055756";
     }
-@Override
+
+    /**
+     *
+     * @param e
+     */
+    @Override
     public void actionPerformed(ActionEvent e) { 
         
         System.out.println(about);
